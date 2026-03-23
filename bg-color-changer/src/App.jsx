58c2bb398c8dd265cbb9589c06d92e3b2f9308ce
manmitsinghchouhan <manmitsinghchouhan2005@gmail.com>
2026-03-23 09:20:24 +0530

    First Project
    This project is basic bg-color-changer
    but from ir, i understand used tailwind CSS and how to create react elements and changer there stare through useState hook

---
+++ bg-color-changer/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("olive");
+
+  return (
+    <div className="w-full h-screen" style={{ backgroundColor: color }}>
+      <div className="fixed bottom-10 inset-x-0 flex justify-center px-2">
+        <div className="flex justify-around gap-3 bg-white px-2 py-2 rounded-xl">
+          <button className="bg-red-500 px-3 py-3 rounded-lg shadow-lg" onClick={()=>{setColor("red")}}>red</button>
+          <button className="bg-blue-500 px-3 py-3 rounded-lg shadow-lg" onClick={()=>{setColor("blue")}}>blue</button>
+          <button className="bg-green-500 px-3 py-3 rounded-lg shadow-lg" onClick={()=>{setColor("green")}}>green</button>
+          <button className="bg-purple-500 px-3 py-3 rounded-lg shadow-lg" onClick={()=>{setColor("purple")}}>purple</button>
+          <button className="bg-pink-500 px-3 py-3 rounded-lg shadow-lg" onClick={()=>{setColor("pink")}}>pink</button>
+          <button className="bg-white px-3 py-3 rounded-lg shadow-lg" onClick={()=>{setColor("white")}}>white</button>
+          <button className="bg-black px-3 py-3 rounded-lg shadow-lg text-cyan-50 " onClick={()=>{setColor("black")}}>black</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
