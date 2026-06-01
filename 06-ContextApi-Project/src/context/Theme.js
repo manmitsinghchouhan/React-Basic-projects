fc4a62ab3992c0e9393d7c58ed6599ea2e4d0500
manmitsinghchouhan <manmitsinghchouhan2005@gmail.com>
2026-06-01 12:38:02 +0530

    Second way of using context API
    understand another way of using context api with theme changer project

---
+++ 06-ContextApi-Project/src/context/Theme.js
@@ -0,0 +1,10 @@
+import { createContext, useContext } from "react";
+
+export const ThemeContext = createContext({
+    theme : "light",
+    darkTheme : ()=>{},
+    lightTheme : ()=>{}
+})
+
+export const ThemeProvider = ThemeContext.Provider
+
